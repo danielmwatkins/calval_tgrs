57fb69736d938e986c39f1e118977809ca54735c
daniel watkins <danielmwatkins@gmail.com>
2026-07-23 12:41:48 -0400

    update plotting methods

--- scripts/plot_bright_ice_water_discrimination.py
+++ scripts/plot_bright_ice_water_discrimination.py
@@ -38,8 +38,9 @@ fc_dataloc = dataloc + 'data/modis/falsecolor/'
 lm_dataloc = dataloc + 'data/validation_dataset/binary_landmask/'
 lb_dataloc = dataloc + 'data/validation_dataset/binary_floes/'
 lf_dataloc = dataloc + 'data/validation_dataset/binary_landfast/'
-ice_mask_loc = '../data/ift_prelim_ice_mask/'
-cloud_mask_loc = '../data/ift_cloud_mask/cleaned/'
+ice_mask_loc = '../data/ift_ice_mask/'
+cloud_mask_loc = '../data/ift_cloud_mask/'
+modis_mask_loc = '../data/cloudfraction_numeric/'
 
 tc_images = {}
 fc_images = {}
@@ -48,9 +49,10 @@ lf_images = {}
 lm_images = {}
 ice_mask_images = {}
 cloud_mask_images = {}
+modis_mask_images = {}
 
 missing = []
-plot_cases = ['011', '022', '108', '128']
+plot_cases = ['011', '063', '108', '128']
 for row, data in df.iterrows():
     cn = data['case_number']
     date = pd.to_datetime(data['start_date']).strftime('%Y%m%d')
@@ -65,10 +67,13 @@ for row, data in df.iterrows():
         lb_images[row] = io.imread(lb_dataloc +  '-'.join([cn, region, date, sat, 'binary_floes' + '.png']))
         lf_images[row] = io.imread(lf_dataloc +  '-'.join([cn, region, date, sat, 'binary_landfast' + '.png']))
         lm_images[row] = io.imread(lm_dataloc +  '-'.join([cn, region, date, sat, 'binary_landmask' + '.png']))
-        ice_mask_images[row] = io.imread(ice_mask_loc +  '-'.join([cn, region, '100km', date, sat, '250m', 'prelim_icemask' + '.png']))
-        cloud_mask_images[row] = io.imread(cloud_mask_loc +  '-'.join([cn, region, '100km', date, sat, '250m', 'cloudmask' + '.png']))
+        ice_mask_images[row] = io.imread(ice_mask_loc +  '-'.join([cn, region, '100km', date, sat, '250m', 'ice_mask' + '.png']))
+        cloud_mask_images[row] = io.imread(cloud_mask_loc +  '-'.join([cn, region, '100km', date, sat, '250m', 'cloud_mask' + '.png']))
+        modis_mask_images[row] = pd.read_csv(modis_mask_loc + '-'.join([cn, region, date, sat, 'cloudfraction.csv']), index_col=0)
+        modis_mask_images[row].index = modis_mask_images[row].index.astype(int)
+        modis_mask_images[row].columns = modis_mask_images[row].columns.astype(int)
 
-fig, ax = pplt.subplots(nrows=3, ncols=4, sharex=False, sharey=True)
+fig, ax = pplt.subplots(nrows=4, ncols=4, sharex=False, sharey=True)
 titles = []
 for i, case_number in enumerate(plot_cases):
     case = case_number + '_aqua'
@@ -90,16 +95,14 @@ for i, case_number in enumerate(plot_cases):
     y, xe = np.histogram(data, bins=np.linspace(0, 255, 64))
     xc = (xe[1:] + xe[:-1]) / 2
 
-    ax[1, i].plot(xc, y / np.sum(y), color='red5', label='Band 1')
+    ax[1, i].plot(xc/255, y / np.sum(y), color='red5', label='Band 1')
     
-    idxmax = y[xc > 75].argmax()
-    peak_loc = xc[xc > 75][idxmax]
-
-    bright_ice = ice_mask & (red_band >= peak_loc)
-    
-    ax[1, i].axvline(75, lw=1, color='red5', ls='--')
-    ax[1, i].axvline(peak_loc, lw=1, color='red5', ls='-.')
-    ax[1, i].axvline(0.5 * (75 + peak_loc), lw=1, color='red5', ls='-')
+    idxmax = y[xc/255 > 0.45].argmax()
+    red_peak_loc = xc[xc/255 > 0.45][idxmax]
+    ice_threshold = 0.5 * (0.45 + red_peak_loc/255)
+    ax[1, i].axvline(0.45, lw=1, color='red5', ls='--')
+    ax[1, i].axvline(red_peak_loc/255, lw=1, color='red5', ls='-.')
+    ax[1, i].axvline(ice_threshold, lw=1, color='red5', ls='-')
     
     ax[1, i].format(ylabel='', xlabel='Reflectance')
 
@@ -112,18 +115,30 @@ for i, case_number in enumerate(plot_cases):
     data = data[data > 0]
     y, xe = np.histogram(data, bins=np.linspace(0, 255, 64))
     xc = (xe[1:] + xe[:-1]) / 2
-
-    ax[1, i].plot(xc, y / np.sum(y), color='blue8', label='Band 2')
+    idxmax = y[xc/255 > 0.45].argmax()
+    nir_peak_loc = xc[xc/255 > 0.45][idxmax]
     
-    idxmax = y[xc > 75].argmax()
-    peak_loc = xc[xc > 75][idxmax]
-    bright_ice = ice_mask & (bright_ice | (nir_band >= peak_loc))
-    # ax[1, i].axvline(75, lw=1, color='k', ls='--')
-    ax[1, i].axvline(peak_loc, lw=1, color='blue8', ls=':')
-    # ax[1, i].axvline(0.5 * (75 + peak_loc), lw=1, color='k', ls='-')
+    ax[1, i].axvline(nir_peak_loc/255, lw=1, color='blue8', ls=':')
+    ax[1, i].plot(xc/255, y / np.sum(y), color='blue8', label='Band 2', zorder=1)
+
+    # Band 7
+    sir_band = fc_images[case][:,:,0].copy()
+    sir_band[cloud_mask > 0] = 0
+    sir_band[land_mask > 0] = 0
+    
+    data = np.ravel(sir_band)
+    data = data[data > 0]
+    y, xe = np.histogram(data, bins=np.linspace(0, 255, 64))
+    xc = (xe[1:] + xe[:-1]) / 2
+
+    ax[1, i].plot(xc/255, y / np.sum(y), color='gold', label='Band 7', zorder=0)
 
     
+    ax[1, i].legend(ncols=1, loc='ur')
 
+    ice_mask = (ice_mask / 255) > ice_threshold
+    bright_ice = ice_mask & ((red_band >= red_peak_loc) | (nir_band >= nir_peak_loc))
+    
     ax[2,i].imshow(np.ma.masked_array(ice_mask, mask=ice_mask > 0), c='blue6')
     ax[2,i].imshow(np.ma.masked_array(ice_mask, mask=ice_mask == 0), c='gray4')
     ax[2,i].imshow(np.ma.masked_array(bright_ice, mask=bright_ice == 0), c='w')
@@ -154,6 +169,9 @@ for i, case_number in enumerate(plot_cases):
         ax[2,i].imshow(np.ma.masked_array(outlines, mask=outlines == 0), c='yellow6')
 
 
+        # MODIS cloud fraction
+    cb = ax[3,i].pcolormesh(modis_mask_images[case].values, vmin=0, vmax=100, N=17, cmap='Blues_r')
+
 for j in [0, 2]:
     for i in range(0, 4):
         ax[j, i].format(xticks='none', yticks='none')
@@ -164,10 +182,12 @@ for color in ['gray4', 'w', 'blue6', 'lilac', 'red5', 'yellow4', 'gray9']:
 ax[2,-1].legend(h, ['IFT Sea Ice', 'IFT Bright Ice', 'IFT Water', 'IFT Cloud', 'Manual Floes', 'Manual Landfast Ice', 'Land'], loc='r', ncols=1)
 
 h = []
-for ls in ['--', '-.', '-']:
-    h.append(ax.plot([],[],m='', lw=1, color='k'))
-ax[1,-1].legend(h, ['Minimum Ice', 'Brightness Peak', 'Ice Threshold'], loc='r', ncols=1)
+for ls, c in zip(['--', '-.', ':', '-'], ['r', 'r', 'b', 'r']):
+    h.append(ax.plot([],[],m='', ls=ls, lw=1, color=c))
+ax[1,-1].legend(h, ['$\\tau_1$', 'Band 1 Peak', 'Band 2 Peak', 'Midpoint Threshold', ], loc='r', ncols=1)
+ax[3, -1].colorbar(cb, label='MODIS Cloud Fraction (%)')
+ax[3,:].format(yreverse=True)
 
 fig.format(abc=True)
-fig.format(toplabels=titles, leftlabels=['True Color (1-4-3)', 'IFT Masks', 'Reflectance PDF'])
-fig.save("../figures/fig_07_ice_water_discrimination.png", dpi=300)
+fig.format(toplabels=titles, leftlabels=['True Color (1-4-3)','Reflectance PDF', 'IFT Masks', 'MODIS Cloud Fraction'])
+fig.save("../figures/fig_05_ice_water_discrimination.png", dpi=300)
