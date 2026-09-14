48f341d15cfedb17c7ee12f2fb1b6a66c18d0409
daniel watkins <danielmwatkins@gmail.com>
2026-09-14 16:47:44 -0400

    work towards cloud handling

--- scripts/dev/segmentation.jl
+++ scripts/dev/segmentation.jl
@@ -431,3 +431,52 @@ function _remove_labels!(output, indices, remove_labels)
 end
 
 
+function colorize_classification(labeled_image;
+color_map=Dict(
+        0=>RGB(0),
+        1=>RGB(0.018, 0.49, 0.64),
+        2=>RGB(1),
+        3=>RGB(0.84, 0.73, 0.94)
+        )
+    )
+    return n0f8.(map(i -> color_map[i], labeled_image))
+end
+
+"""
+
+Produces a segmented image with up to 4 categories: land, water, ice, and cloud.
+
+"""
+function ift_classification(false_color_image, land_mask;
+        tau_1=0.1,
+        tau_2=0.2,
+        tau_7=0.2,
+        label_map=Dict("land"=>0, "water"=>1, "ice"=>2, "cloud"=>3)
+    )
+    
+    cloud_mask_algorithm=Watkins2026CloudMask(
+        band_7_threshold = tau_7,
+        band_2_threshold = tau_2,
+        opening_strel = strel_disk(3),
+        dilation_strel = strel_disk(2),
+        min_hole_size = 300,
+        max_fill_size = 1e4,
+        min_contrast = 0.2,
+        ) 
+
+    ice_mask_algorithm=IceDetectionBrightnessMidpoint(minimum_reflectance=tau_1)
+    
+    coastal_buffer = create_coastal_buffer_mask(land_mask .> 0, strel_disk(5))
+    fc_masked = apply_landmask(false_color_image, coastal_buffer)
+    clouds = cloud_mask_algorithm(fc_masked) .> 0
+    band_1_masked = Gray.(blue.(apply_landmask(fc_masked, clouds)))
+    ice = ice_mask_algorithm(band_1_masked) .> 0
+    
+    classified_image = ones(Int64, size(false_color_image)) .* label_map["water"]
+    classified_image[coastal_buffer] .= label_map["land"]
+    classified_image[ice] .= label_map["ice"]
+    classified_image[clouds] .= label_map["cloud"]
+    
+    return classified_image
+end
+
