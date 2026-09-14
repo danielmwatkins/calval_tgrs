19852b173885a2781f7beb490a6ef1f0db61e43b
daniel watkins <danielmwatkins@gmail.com>
2026-09-14 14:10:32 -0400

    update scripts, illustration

--- scripts/compute_classification.jl
+++ scripts/compute_classification.jl
@@ -8,6 +8,17 @@ dataset = Watkins2026Dataset(ref="main")
 fc_imgs = modis_falsecolor.(dataset)
 land_masks = modis_landmask.(dataset) .|> r -> Gray.(r) .> 0
 
+function _colorize_classification(labeled_image;
+color_map=Dict(
+        0=>RGB(0),
+        1=>RGB(0.018, 0.49, 0.64),
+        2=>RGB(1),
+        3=>RGB(0.84, 0.73, 0.94)
+        )
+    )
+    return map(i -> color_map[i], labeled_image)
+end
+
 """
 
 Produces a segmented image with up to 4 categories: land, water, ice, and cloud.
@@ -32,7 +43,7 @@ function ift_classification(false_color_image, land_mask;
 
     ice_mask_algorithm=IceDetectionBrightnessMidpoint(minimum_reflectance=tau_1)
     
-    coastal_buffer = create_coastal_buffer_mask(land_mask .> 0, strel_disk(25))
+    coastal_buffer = create_coastal_buffer_mask(land_mask .> 0, strel_disk(5))
     fc_masked = apply_landmask(false_color_image, coastal_buffer)
     clouds = cloud_mask_algorithm(fc_masked) .> 0
     band_1_masked = Gray.(blue.(apply_landmask(fc_masked, clouds)))
@@ -43,12 +54,11 @@ function ift_classification(false_color_image, land_mask;
     classified_image[ice] .= label_map["ice"]
     classified_image[clouds] .= label_map["cloud"]
     
-    return SegmentedImage(false_color_image, classified_image)
+    return classified_image
 end
 
-classified = ift_classifiation.(fc_imgs, land_masks)
-images = view_seg_random.(classified)
-data = labels_map.(classified)
+classified = ift_classification.(fc_imgs, land_masks)
+images = _colorize_classification.(classified)
 
 file_names = [
      join(
@@ -71,6 +81,10 @@ for (idx, data) in enumerate(eachrow(dataset.info))
             data.satellite,
             "classified.png"
         ], "-")
-    save(joinpath("../data/classification_results/images", fname, classified[idx]))
-    save(joinpath("../data/classification_results/", fname, data[idx]))    
+    save(joinpath("../data/classification_results/images", fname), images[idx])
+    # Test: what methods work for saving the raw values?
+    # try
+    #     save(joinpath("../data/classification_results/", fname), view_seg(SegmentedImage(Gray.(data[idx] ./ 4), data[idx])))     catch
+    #     println(fname, " failed")
+    # end
 end
